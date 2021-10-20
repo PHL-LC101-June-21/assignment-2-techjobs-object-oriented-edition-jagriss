b9be806e419b13aab62b4d155df0a85f07ff6090
Jace Grissom <jacegrissom43@gmail.com>
2021-10-20 13:58:15 -0400

    initial commit of finished task 2 with passing tests

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -3,6 +3,17 @@ package org.launchcode.techjobs.oo;
 import java.util.Objects;
 
 public class CoreCompetency {
+    public String getValue() {
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 
     private int id;
     private static int nextId = 1;
